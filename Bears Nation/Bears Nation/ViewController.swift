3218364ec5687dc1353e073c9b41d760111d6477
Nao Yanase <nao.yanase@wustl.edu>
2020-11-20 18:04:12 -0600

    init

---
+++ Bears Nation/Bears Nation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bears Nation
+//
+//  Created by Nao Yanase on 11/20/20.
+//  Copyright © 2020 WashU Athletics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
