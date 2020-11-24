33fe38109c5ae1f32b04b16ec95a3ce9ae3c75a9
Nao Yanase <nao.yanase@wustl.edu>
2020-11-24 17:56:15 -0600

    test

--- Bears-Nation/Bears-Nation/ViewController.swift
+++ Bears-Nation/Bears-Nation/ViewController.swift
@@ -8,7 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    let dummyArticleNames = ["Title One", "Title Two", "Title Three", "Title Four"]
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dummyArticleNames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+    
+    func setupCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
