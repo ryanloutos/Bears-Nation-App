86126b062342b570103c6ffb6d7485ccd38017a9
boanderson <46663503+boanderson@users.noreply.github.com>
2020-12-04 22:14:26 -0500

    BoUpdate

---
+++ Bears-Nation/Bears-Nation/IndAthleteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  IndAthleteViewController.swift
+//  Bears-Nation
+//
+//  Created by Bo Anderson on 12/4/20.
+//  Copyright © 2020 WashU Athletics. All rights reserved.
+//
+
+import UIKit
+
+class IndAthleteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
